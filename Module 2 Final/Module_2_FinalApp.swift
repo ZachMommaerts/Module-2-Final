0574adccca67f0443a1a0ad63320aca8e5fa73b6
ZachMommaerts <zmommaerts@gmail.com>
2023-03-09 10:49:55 -0600

    Initial Commit

---
+++ Module 2 Final/Module_2_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Module_2_FinalApp.swift
+//  Module 2 Final
+//
+//  Created by Zach Mommaerts on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Module_2_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
